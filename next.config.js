65ef33b133e87448dfad107c27a3b734ef1ccc0b
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-07-29 00:46:11 -0400

    Pre-deploy config 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/ubiobio.dev',
+  assetPrefix: '/ubiobio.dev/',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
